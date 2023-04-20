ad2c1bbaece791fe65187440d6683722bccd7731
mitchell <mitchellstevens101101@gmail.com>
2023-04-20 14:55:24 -0500

    This is almost the final public-ready version of the program. Still have some decisions to make in the "Modes" and "Minor" preferences error detection.

---
+++ app/src/main/java/com/mitchell/scalepicker/MainActivity.kt
@@ -0,0 +1,134 @@
+/*TODO: Now begin work on implementing Modes in settings.
+  TODO: Use what you have already built in SettingsActivity
+  TODO: To reduce overhead and be as efficient as possible
+ */
+/*TODO: Modes have been implemented
+  TODO: Now do error checking to make sure at least one mode
+  TODO: (And one of each type of selected scale types) is required to be selected
+  TODO: similar to pitch error checking
+
+  TODO: Maybe make is so that if all dependence switches are disabled (melodic minor, natural minor etc..)
+  TODO: then the parent switch (Minor) is disabled
+ */
+/*TODO: There is a bug in which the scale displayed on screen is lost
+  TODO: when the screen rotates. Please fix.
+ */
+/*TODO: I would like to add a feature in the preferences screen where
+  TODO: users can long press a pitch and all other pitches besides that one
+  TODO: will be disabled.
+ */
+package com.mitchell.scalepicker
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.WindowManager
+import android.widget.Button
+import android.widget.TextView
+import androidx.preference.PreferenceManager
+import java.util.Vector
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //shared preference and condition to set or unset "Screen Always On"
+        val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this)
+        if(sharedPrefs.getBoolean("display_always_on", false)) window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        else window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
+        //get enabled note names and store in a vector to be passed to notePicker
+        val enabledNoteNames = Vector<String>(0)
+        val noteNamesArr = resources.getStringArray(R.array.note_names)
+        for (i in (0..11)) if(sharedPrefs.getBoolean("pitch_$i", true)) enabledNoteNames.add(noteNamesArr[i])
+
+        //get enabled modes and store in vector to be passed to modePicker
+        val enabledModeNames = Vector<String>(0)
+        if (sharedPrefs.getBoolean("major", true)) enabledModeNames.add("Major") //if major is enabled, add major. (modeNamesArr[0] is major)
+        if (sharedPrefs.getBoolean("minor", true)){
+            if(sharedPrefs.getBoolean("minor_natural", true)) enabledModeNames.add("Natural Minor")
+            if(sharedPrefs.getBoolean("minor_harmonic", true)) enabledModeNames.add("Harmonic Minor")
+            if(sharedPrefs.getBoolean("minor_melodic", true)) enabledModeNames.add("Melodic Minor")
+        }
+        if (sharedPrefs.getBoolean("modes", false)){
+            if(sharedPrefs.getBoolean("modes_dorian", true)) enabledModeNames.add("Dorian")
+            if(sharedPrefs.getBoolean("modes_phrygian", true)) enabledModeNames.add("Phrygian")
+            if(sharedPrefs.getBoolean("modes_lydian", true)) enabledModeNames.add("Lydian")
+            if(sharedPrefs.getBoolean("modes_mixolydian", true)) enabledModeNames.add("Mixolydian")
+            if(sharedPrefs.getBoolean("modes_locrian", false)) enabledModeNames.add("Locrian")
+        }
+
+        //defining TextView objects to be updated by RNG
+        val pitchClassText: TextView = findViewById(R.id.scale_pitch) //Text for letter name to display. (A, C#, Bb etc)
+        val modeText: TextView = findViewById(R.id.scale_mode)  //Text for mode. (Major, Harmonic Minor, Phrygian, etc)
+        val randomButton: Button = findViewById(R.id.button)   //Button to generate scale
+
+        //this block handles the randomButton being tapped
+        randomButton.setOnClickListener{
+            pitchClassText.text = notePicker(enabledNoteNames)
+            modeText.text = modePicker(enabledModeNames)
+        }
+    }
+
+    //Function to return a random note name
+    private fun notePicker(noteNames: Vector<String>): String {
+        //commented lines below are original function
+        //val noteNames = arrayOf("C", "C♯/D♭", "D", "D♯/E♭", "E", "F", "F♯/G♭", "G", "G♯/A♭", "A", "A♯/B♭", "B")
+        //return noteNames.getString((0..11).random())
+        //val noteNames: Array<String> = resources.getStringArray(R.array.note_names)
+
+       // return noteNames.elementAt((0..noteNames.count()).random() - 1)
+        return noteNames.random()
+    }
+
+    //Function to return a random mode
+    private fun modePicker(modeNames: Vector<String>): String {
+        return modeNames.random()
+/*
+        when ((0..2).random()) { //logic to select scales at random.
+            0 -> return "Major"
+            1 -> {
+                when((0..2).random()){
+                    0 -> return "Natural Minor"
+                    1 -> return "Harmonic Minor"
+                    2 -> return "Melodic Minor"
+                }
+            }
+            2 -> {
+                when((0..3).random()){
+                    0 -> return "Dorian"
+                    1 -> return "Phrygian"
+                    2 -> return "Lydian"
+                    3 -> return "Mixolydian"
+                }
+            }
+        }
+    return "error" // this return statement should never be reached
+*/
+    }
+
+    //function to create the AppBar
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_main_activity, menu)
+        return true
+    }
+
+    //function to handle button in AppBar being clicked
+    override fun onOptionsItemSelected(item: MenuItem): Boolean{
+        when (item.itemId) {
+            R.id.action_settings -> {
+                val settingsIntent = Intent(this, SettingsActivity::class.java)
+                startActivity(settingsIntent)
+                return true
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
+
+
+    }
+}
---
+++ app/src/main/java/com/mitchell/scalepicker/SettingsActivity.kt
@@ -0,0 +1,155 @@
+package com.mitchell.scalepicker
+
+import android.content.Intent
+import android.content.SharedPreferences
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener
+import android.os.Bundle
+import android.view.KeyEvent
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager.getDefaultSharedPreferences
+
+class SettingsActivity : AppCompatActivity(){
+
+    //These are global so they can be accessed and defined throughout ALL functions and subclasses
+    private lateinit var preferences: SharedPreferences
+    private lateinit var listener: OnSharedPreferenceChangeListener
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings_activity)
+
+        //defining here so registering onSharedPreferenceChangeListener doesnt get pissy
+        preferences = getDefaultSharedPreferences(this)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.settings, SettingsFragment())
+                .commit()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        prefsManager(preferences) //call function to handle "Enable All Pitches" button
+        preferences.registerOnSharedPreferenceChangeListener(listener)
+
+
+    }
+
+    //Function to prevent user from deselecting ALL pitches. At least ONE needs to be selected.
+    //Keeping this code in a separate function instead of onCreate seems to be better practice
+    //And allows for smoother UI regeneration with recreate()
+    private fun prefsManager(preferences: SharedPreferences){
+       // preferences = PreferenceManager.getDefaultSharedPreferences(this)
+        listener = OnSharedPreferenceChangeListener { sharedPreferences, key ->
+            //Defining prefsEditor here so it can be defined once and used many times by function
+            val prefsEditor = sharedPreferences.edit()
+
+            //Error checking for pitches
+            var numOfSelectedPitches = 0
+            for(i in 0..11) if(preferences.getBoolean("pitch_$i", true)) numOfSelectedPitches++
+            if (numOfSelectedPitches == 0) {
+                Toast.makeText(this,"At least one pitch must be selected", Toast.LENGTH_SHORT).show()
+                prefsEditor.putBoolean(key,true)
+                prefsEditor.commit()
+                recreate()
+            }
+
+                //Error checking for scale types
+            //First ensure that at least one scale type is selected
+            if(!preferences.getBoolean("major", true) &&
+                !preferences.getBoolean("minor", true) &&
+                !preferences.getBoolean("modes", false)){
+
+                Toast.makeText(this, "At least one scale type must be selected", Toast.LENGTH_SHORT).show()
+                prefsEditor.putBoolean(key, true)
+                prefsEditor.commit()
+                recreate()
+            }
+
+            //If that check passes, now ensure at least one of each sub-category is selected
+            //No need to worry about major here because that is dealt with in above code
+            else if (!preferences.getBoolean("minor_natural", true) &&
+                     !preferences.getBoolean("minor_harmonic", true) &&
+                     !preferences.getBoolean("minor_melodic", true) ||
+                     !preferences.getBoolean("modes_dorian", true) &&
+                     !preferences.getBoolean("modes_phrygian", true) &&
+                     !preferences.getBoolean("modes_lydian", true) &&
+                     !preferences.getBoolean("modes_mixolydian", true) &&
+                     !preferences.getBoolean("modes_locrian", false)){
+
+                //Doing something different in the minor and modal sections so I can survey people
+                //about which is less confusing
+
+                //this one will be like pitch behavior
+                //If all minor scales are deselected
+                if (key.substring(0,5) == "minor"){
+                    Toast.makeText(this, "At least one minor type must be selected, or minor disabled.", Toast.LENGTH_SHORT).show()
+                    prefsEditor.putBoolean(key, true)
+                    prefsEditor.commit()
+                    recreate()
+                }
+
+                //this one will disable the category if the user attempts to deselect everything
+                //if all modal scales are deselected
+                else {
+                    prefsEditor.putBoolean(key, true)
+                    prefsEditor.putBoolean("modes", false)
+                    prefsEditor.commit()
+                    recreate()
+                }
+            }
+        }
+    }
+
+
+
+
+    class SettingsFragment : PreferenceFragmentCompat(){
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+            //block of code to handle the "Enable All Pitches button"
+            //requireActivity() is the magic function for context in this class
+            //TODO: when adding an "Enable all modes" button, this may be a great place to add it
+            val noValuePreference: Preference? = findPreference("enable_all_pitches") //this may be better as a when (switch) statement when adding modal code
+            noValuePreference?.setOnPreferenceClickListener {
+                val preferences = getDefaultSharedPreferences(requireActivity())
+                val prefsEditor = preferences.edit()
+                for (i in (0..11)){
+                    prefsEditor.putBoolean("pitch_$i", true)
+                }
+                prefsEditor.commit()
+                Toast.makeText(requireActivity(), "All Pitches Enabled", Toast.LENGTH_SHORT).show()
+                requireActivity().recreate()
+                true
+            }
+        }
+    }
+
+    //makes the OS back button act like the application's UP button
+    //This should allow preferences to work as expected when using back button
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK){
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+
+    }
+
+    //these overrides are here as good practice, keeps listeners from hanging when activity isnt loaded.
+    //Re-registers when activity resumes
+    override fun onPause() {
+        super.onPause()
+        preferences.unregisterOnSharedPreferenceChangeListener(listener)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        preferences.registerOnSharedPreferenceChangeListener(listener)
+    }
+
+}
